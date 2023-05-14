4f07204f676612a245f6edd2fcc78a6d4d518178
dipaktapase <dipaktapase1512@gmail.com>
2023-05-14 16:06:02 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Home from './pages/Home'
-import './App.css';
+import Home from "./pages/Home.js";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="max-w-screen-md mx-auto pt-20">
-      {/* App Component */}
-      <Home />
-    </div>
+    <Router>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" Component={Home} />
+          {/* <Route path="/" element={<Home />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
