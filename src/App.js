8d6de4da5fda9941eab3183eca76c0177e302380
dipaktapase <dipaktapase1512@gmail.com>
2023-05-17 12:52:25 +0530

    created pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import Home from "./pages/Home.js";
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Article from "./pages/Article.js";
+import ArtilclesList from "./pages/ArticlesList.js";
+
 
 function App() {
   return (
-    <Router>
-      <div className="max-w-screen-md mx-auto pt-20">
+    <div className="max-w-screen-md mx-auto pt-20">
+      <Router>
         <Routes>
-          <Route path="/" Component={Home} />
           {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" Component={Home} />
+          <Route path="/about" Component={About} />
+          <Route path="/artilce" Component={Article} />
+          <Route path="/articles-list" Component={ArtilclesList} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ArtilclesList = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold mb-6 text-grey-900">
+        ArtilclesList
+      </h1>
+    </div>
+  );
+};
+
+export default ArtilclesList;
