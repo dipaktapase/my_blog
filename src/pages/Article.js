17d381a4ead5b2af9cb4bfa9463322f7d5b42883
dipaktapase <dipaktapase1512@gmail.com>
2023-05-17 14:58:38 +0530

    added article content

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import articles from "./article-content";
 
 const Article = () => {
   const name = useParams();
+  const article = articles.find((article) => article.name === name.name);
+  if (!article) return <h1>Article does not exist.</h1>
+  
   return (
     <div className="mb-20">
       <h1 className="sm:text-4xl text-2xl font-bold mb-6 text-grey-900">
-        This is {name.name} Article
+        {article.title}
       </h1>
+      {article.content.map((paragraph, index) => (
+        <p className="mx-auto leading-relaxed text-base mb-4" key={index}>
+          {paragraph}
+        </p>
+      ))}
     </div>
   );
 };
---
+++ src/pages/article-content.js
@@ -0,0 +1,19 @@
+const articles = [
+  {
+    name: "learn-react",
+    title: "The fastest way to learn React",
+    content: ["Hello Welcome to this article for learning react quick."],
+  },
+  {
+    name: "learn-web",
+    title: "The fastest way to learn web",
+    content: ["Hello Welcome to this article for learning web quick."],
+  },
+  {
+    name: "learn-js",
+    title: "The fastest way to learn js",
+    content: ["Hello Welcome to this article for learning js quick."],
+  },
+];
+
+export default articles;
