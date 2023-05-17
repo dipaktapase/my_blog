0045f972069257ddadbfb6bf8eb7affc3b72d27a
dipaktapase <dipaktapase1512@gmail.com>
2023-05-17 15:59:21 +0530

    added articleList, fixed images

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -6,13 +6,13 @@ const ArtilclesList = () => {
   return (
     <>
       <h1 className="sm:text-4xl text-2xl font-bold mb-6 text-grey-900">
-        Artilcle List
+        Article List
       </h1>
       <div className="container py-4 mx-auto ">
         <div className="flex flex-wrap -m-4">
           {articles.map((article, index) => (
             <div key={index} className="p-4 md:w-1/2">
-              <div>
+              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <Link to={`/article/${article.name}`}>
                   <img
                     className="lg:h-48 md:h-36 w-full object-cover object-center"
@@ -26,6 +26,17 @@ const ArtilclesList = () => {
                       {article.title}
                     </h3>
                   </Link>
+                  <p className="leading-relaxed mb-3">
+                    {article.content[0].substring(0, 115)}...
+                  </p>
+                  <div className="flex items-center flex-wrap">
+                    <Link
+                      className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                      to={`/article/${article.name}`}
+                    >
+                      Learn More
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -2,19 +2,19 @@ const articles = [
   {
     name: "learn-react",
     title: "The fastest way to learn React",
-    thumbnail: '../../public/images/blog1.jpg',
+    thumbnail: "/images/blog1.jpg",
     content: ["Hello Welcome to this article for learning react quick."],
   },
   {
     name: "learn-web",
     title: "The fastest way to learn web",
-    thumbnail: './images/blog2',
+    thumbnail: "./images/blog2.jpg",
     content: ["Hello Welcome to this article for learning web quick."],
   },
   {
     name: "learn-js",
     title: "The fastest way to learn js",
-    thumbnail: './images/blog3',
+    thumbnail: "./images/blog3.jpg",
     content: ["Hello Welcome to this article for learning js quick."],
   },
 ];
