bbba309e0b0e121f5a49a42a794251c5dba40af6
dipaktapase <dipaktapase1512@gmail.com>
2023-05-17 15:26:52 +0530

    added articles in articleList

--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -2,16 +2,19 @@ const articles = [
   {
     name: "learn-react",
     title: "The fastest way to learn React",
+    thumbnail: '../../public/images/blog1.jpg',
     content: ["Hello Welcome to this article for learning react quick."],
   },
   {
     name: "learn-web",
     title: "The fastest way to learn web",
+    thumbnail: './images/blog2',
     content: ["Hello Welcome to this article for learning web quick."],
   },
   {
     name: "learn-js",
     title: "The fastest way to learn js",
+    thumbnail: './images/blog3',
     content: ["Hello Welcome to this article for learning js quick."],
   },
 ];
